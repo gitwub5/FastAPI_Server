78d22c480d9d474469b88a8a573e0b0577542ace
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-05-10 21:58:18 +0900

    router refactor

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 from app.api.routes import example_router
-from app.api.routes import items
+from app.api.routes import router
 
 router = APIRouter()
 
-#라우터 생길 시 추가
-router.include_router(example_router.router, prefix="/example", tags=["example"])
-router.include_router(items.router)
+router.include_router(router.router)
